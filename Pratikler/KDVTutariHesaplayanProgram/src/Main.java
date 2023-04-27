3fc4d0b5e393a86f7727310b4a16bd3d08c3ad53
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-04-27 19:30:34 +0300

    proje tamamlandi

--- Pratikler/KDVTutariHesaplayanProgram/src/Main.java
+++ Pratikler/KDVTutariHesaplayanProgram/src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //1.Adim : Kullanicidan deger al
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ucret tutarini giriniz : ");
+        double tutar = scanner.nextDouble();
+
+        final double KDV_ORAN = 0.18;
+
+        System.out.println("KDV orani : " + KDV_ORAN);
+
+        //2.Adim : KDV tutarini hesapla
+
+        double kdvTutar = tutar * KDV_ORAN;
+        System.out.println("KDV tutari : " + kdvTutar);
+
+        //3.Adim : toplam tutari hesapla
+
+        double toplamTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz tutari : " + tutar);
+        System.out.println("KDV'li tutari : " + toplamTutar);
     }
 }
