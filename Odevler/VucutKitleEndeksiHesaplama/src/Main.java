46610f10c2d9b7c52ba6497f3278c325195830b6
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-04 14:30:30 +0300

    odev tamamlandi

--- Odevler/VucutKitleEndeksiHesaplama/src/Main.java
+++ Odevler/VucutKitleEndeksiHesaplama/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        System.out.println("Vucut Kitle Endeksi Hesaplama Uygulamamiza Hosgeldiniz!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lutfen boyunuzu metre cinsinden giriniz : ");
+        double boy = scanner.nextDouble();
+        System.out.print("Lutfen kilonuzu giriniz : ");
+        double kilo = scanner.nextDouble();
+
+        double endeks = kilo / Math.pow(boy,2);
+
+        System.out.println("Vucut Kitle Endeksiniz : " + endeks);
     }
 }
