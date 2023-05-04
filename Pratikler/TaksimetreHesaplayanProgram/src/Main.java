342881accbcadf7fc14902a3463acc53a98d612d
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-04 13:57:52 +0300

    proje tamamlandi

--- Pratikler/TaksimetreHesaplayanProgram/src/Main.java
+++ Pratikler/TaksimetreHesaplayanProgram/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        // Taksimetre Km basine : 2.20 TL tutuyor
+        // Minimum odenecek tutar 20TL dir. 20 tl nin altindaki ucretlerde yine 20tl alinacaktir
+        // Taksimetre acilis ucreti 10TL dir
+
+        double kmTutar = 2.20;
+        double minTutar = 20;
+        double acilis = 10;
+        double total = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Mesafeyi km cinsinden giriniz : ");
+        int kmInput = scanner.nextInt();
+
+        total += (kmTutar * kmInput) + acilis;
+        total = (total < 20) ? 20 : total;
+
+        System.out.println("Toplam tutar : " + total);
+
+
     }
 }
