6f96be1869b6bab811caede236702405456ae968
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-04 14:04:53 +0300

    proje tamamlandi

--- Pratikler/DaireninAlaniVeCevresiniHesaplayanProgram/src/Main.java
+++ Pratikler/DaireninAlaniVeCevresiniHesaplayanProgram/src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lutfen yaricap degerini giriniz : ");
+        int yaricap = scanner.nextInt();
+
+        final double PI = 3.14;
+
+        double alan = PI * Math.pow(yaricap,2);
+        double cevre = 2 * PI * yaricap;
         
+        System.out.println("Dairenin Alani : " + alan);
+        System.out.println("Dairenin Cevresi : " + cevre);
     }
 }
