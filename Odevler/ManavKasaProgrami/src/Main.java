4a6e9984e99aca0876f23b16534d03594314e7b7
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-04 17:14:08 +0300

    proje tamamlandi

--- Odevler/ManavKasaProgrami/src/Main.java
+++ Odevler/ManavKasaProgrami/src/Main.java
@@ -1,6 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        // Meyveler ve Kg fiyatlari
+
+        double armut = 2.14;
+        double elma =  3.67;
+        double domates = 1.11;
+        double muz = 0.95;
+        double patlican = 5.00;
+
+        double toplam = 0;
+
+        System.out.println("Marketimize Hosgeldiniz!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lutfen kac kilo armut almak istediginizi giriniz : ");
+        int armutAdet = scanner.nextInt();
+
+        toplam += armut * armutAdet;
+
+        System.out.print("Lutfen kac kilo elma almak istediginizi giriniz : ");
+        int elmaAdet = scanner.nextInt();
+
+        toplam += elma*elmaAdet;
+
+        System.out.print("Lutfen kac kilo domates almak istediginizi giriniz : ");
+        int domatesAdet = scanner.nextInt();
+
+        toplam += domates*domatesAdet;
+
+        System.out.print("Lutfen kac kilo muz almak istediginizi giriniz : ");
+        int muzAdet = scanner.nextInt();
+
+        toplam += muz*muzAdet;
+
+        System.out.print("Lutfen kac kilo patlican almak istediginizi giriniz : ");
+        int patlicanAdet = scanner.nextInt();
+
+        toplam += patlican*patlicanAdet;
+
+        System.out.println("Toplam Tutar : " + toplam);
     }
 }
