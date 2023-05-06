72b7b9f7a0f77a203bce4f911d7735c912f4a986
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-06 23:22:15 +0300

    kullanici girisi projesi tamamlandi

--- Pratikler/KullaniciGirisi/src/Main.java
+++ Pratikler/KullaniciGirisi/src/Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        String username;
+        String password;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kullanici adinizi giriniz : ");
+        username = scanner.nextLine();
+        System.out.print("Sifrenizi giriniz : ");
+        password = scanner.nextLine();
+
+        if (username.equals("patika") && password.equals("java123")) {
+            System.out.println("Giris yaptiniz!");
+        } else {
+            System.out.println("Bilgileriniz yanlis!");
+            System.out.println("Sifrenizi Degistirmek Istiyorsaniz 1 e istemiyorsaniz 2 ye basin : ");
+            int secim = scanner.nextInt();
+            scanner.nextLine();
+
+            if (secim == 1){
+                System.out.print("Lutfen yeni sifrenizi giriniz : ");
+                String yeniSifre = scanner.nextLine();
+
+                if (yeniSifre.equals(password)){
+                    System.out.println("Sifreniz olusturulamadi!");
+                }else {
+                    System.out.println("Sifreniz olusturuldu! Yeni sifreniz : " + yeniSifre);
+                }
+            }
+            if (secim == 2){
+                System.out.println("Basari ile cikis yaptiniz!");
+            }
+        }
     }
 }
