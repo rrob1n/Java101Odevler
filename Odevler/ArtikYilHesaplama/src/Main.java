749a75748e664541d734afc7575d3123ef81a3cf
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-10 17:53:03 +0300

    Artik Yil Hesaplama adli proje tamamlandi

--- Odevler/ArtikYilHesaplama/src/Main.java
+++ Odevler/ArtikYilHesaplama/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Yil giriniz : ");
+        int year = scanner.nextInt();
+
+        if (year % 4 == 0 || year % 400 == 0) {
+            System.out.println(year + " bir artik yildir !");
+        } else {
+            System.out.println(year + " bir artik yil degildir !");
+        }
     }
 }
