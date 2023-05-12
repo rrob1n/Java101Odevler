11aaddf10d9eb2dc504e842a4c9e03f12f49cf9f
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-12 16:32:01 +0300

    Tek Sayilari Bulan Program adli proje tamamlandi

--- Pratikler/TekSayilarinToplaminiBulanProgram/src/Main.java
+++ Pratikler/TekSayilarinToplaminiBulanProgram/src/Main.java
@@ -1,6 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        // 1. Part : Hocanin yaptigi sekil
+
+        int sayi = 0;
+        int toplam = 0;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Lutfen sayi giriniz : ");
+            sayi = scanner.nextInt();
+
+            if (sayi % 2 == 1) {
+                toplam += sayi;
+            }
+
+        } while (sayi > 0);
+
+        System.out.println("Girdiginiz tek sayilarin toplami : " + toplam);
+
+
+        // 2. Part : Odev
+
+        System.out.println("------------------------------------");
+
+        int sayi1 = 0;
+        int toplam1 = 0;
+
+        do {
+
+            System.out.print("Lutfen Sayi giriniz : ");
+            sayi1 = scanner.nextInt();
+
+            if (sayi1 % 4 == 0) {
+                toplam1 += sayi1;
+            }
+
+
+        } while (sayi1 % 2 != 1);
+
+        System.out.println("Girdiginiz sayilardan 4 un kati olanlarin toplami : " + toplam1);
+
     }
 }
