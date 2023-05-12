64d23730cf6e560f32b8eaf0439c66929d36d8c3
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-12 16:08:52 +0300

    Girilen sayiya kadar olan cift sayilari bulan program adli proje tamamlandi

--- Pratikler/GirilenSayiyaKadarOlanCiftSayilariBulanProgram/src/Main.java
+++ Pratikler/GirilenSayiyaKadarOlanCiftSayilariBulanProgram/src/Main.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        // 1. Part : Hocanin yaptigi bolum
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sayi giriniz : ");
+        int sayi = scanner.nextInt();
+
+        for (int i = 0; i < sayi; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        // 2. Part : Odev
+
+        System.out.println("----------------------------------");
+        System.out.print("Sayi giriniz : ");
+        int sayi2 = scanner.nextInt();
+        int toplam = 0;
+        int counter = 0;
+
+        for (int i = 0; i < sayi2; i++) {
+
+            if (i % 3 == 0 && i % 4 == 0 && i != 0) {
+                System.out.println(i + " sayisi 3 ve 4 e tam bolunur.");
+                toplam += i;
+                counter += 1;
+            }
+        }
+
+        System.out.println("Ortalama : " + (toplam / counter));
+
+
     }
 }
