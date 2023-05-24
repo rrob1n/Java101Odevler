75c6d534232c504cb589a8ff5c0b28e7f3c47036
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-24 15:43:05 +0300

    girilen sayidan kucuk ikinin kuvvetlerinin bulan program adli proje tamamlandi

--- Pratikler/GirilenSayidanKucukIkininKuvvetleriniBulanProgram/src/Main.java
+++ Pratikler/GirilenSayidanKucukIkininKuvvetleriniBulanProgram/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz : ");
+        int sayiInput = scanner.nextInt();
+
+        for (int i = 1; i < sayiInput; i *= 2) {
+            System.out.println(i);
+        }
+
+        // Odev : Girilen sayiya kadar 4 ve 5 in kuvvetlerini ekrana yazdir
+
+        System.out.print("Bir sayi giriniz : ");
+        int sayiInput2 = scanner.nextInt();
+
+        for (int i = 20; i < sayiInput2; i += 4) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
