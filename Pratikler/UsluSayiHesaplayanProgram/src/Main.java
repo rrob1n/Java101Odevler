e44a69c67d16539bccf00cf86143df9fb9bf57f8
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-25 20:08:48 +0300

    Uslu sayi hesaplayan program adli proje tamamlandi.

--- Pratikler/UsluSayiHesaplayanProgram/src/Main.java
+++ Pratikler/UsluSayiHesaplayanProgram/src/Main.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lutfen sayiyi giriniz : ");
+        int sayiInput = scanner.nextInt();
+        System.out.print("Lutfen ussu giriniz : ");
+        int us = scanner.nextInt();
+
+        int sonuc = 1;
+        int i = 0;
+        while(i < us){
+            i++;
+
+            sonuc *= sayiInput;
+        }
+
+        System.out.println("Sonuc : " + sonuc);
+
+
+        // Odev : for dongusu ile yap.
+
+        System.out.print("Lutfen sayiyi giriniz : ");
+        int sayiInput1 = scanner.nextInt();
+        System.out.print("Lutfen ussu giriniz : ");
+        int us1 = scanner.nextInt();
+
+        int toplam = 1;
+        for (int j = 0; j < us1 ; j++) {
+            toplam *= sayiInput1;
+        }
+
+        System.out.println("Sonuc : " + toplam);
     }
 }
