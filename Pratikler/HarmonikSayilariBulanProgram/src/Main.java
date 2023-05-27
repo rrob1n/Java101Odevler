b8281b7dfc3e15da843ca60165e4e74078d3296e
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-27 10:19:04 +0300

    Harmonik Sayilari Bulan Program adli proje tamamlandi.

--- Pratikler/HarmonikSayilariBulanProgram/src/Main.java
+++ Pratikler/HarmonikSayilariBulanProgram/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sayi giriniz : ");
+        int sayiInput = scanner.nextInt();
+
+        double sonuc = 0;
+        for (int i = 1; i <= sayiInput ; i++) {
+            sonuc +=  (1.0/i);
+        }
+
+        System.out.println("Islemin sonucu : " + sonuc);
     }
 }
