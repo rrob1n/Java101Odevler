dcc5be34da0a1480ef5c0e09e89c497298a70565
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-27 11:26:13 +0300

    Yildizlar Ile Ucgen Yapimi adli proje tamamlandi.

--- Pratikler/YildizlarIleUcgenYapimi/src/Main.java
+++ Pratikler/YildizlarIleUcgenYapimi/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ucgenin satir sayisini giriniz : ");
+        int satir = scanner.nextInt();
+
+        for (int i = 1; i <= satir; i++) {
+            for (int j = 1; j <= (satir - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Odev : Elmas yap(yukaridaki ucgenin tersini ekleyecegim ve elmas olacak)
+
+        for (int i = 0; i < satir; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (satir - i) * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
     }
 }
