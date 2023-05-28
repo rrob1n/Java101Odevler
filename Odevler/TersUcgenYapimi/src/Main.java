2ab1ef69ec2675f251e45713bb8d45bfe747b8e7
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-28 15:17:45 +0300

    Ters Ucgen Yapimi adli proje tamamlandi.

--- Odevler/TersUcgenYapimi/src/Main.java
+++ Odevler/TersUcgenYapimi/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Ters Ucgen Yapimina Hosgeldiniz!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kac basamakli ters ucgen olusturmak istersiniz giriniz : ");
+        int basamak = scanner.nextInt();
+
+
+        for (int i = 0; i <= basamak; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (basamak - i) * 2 - 1; k++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
     }
 }
