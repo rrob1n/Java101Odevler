e742744b9ac5b8f4ced35ece267f4cac3a35fde2
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-28 15:31:38 +0300

    Asal Sayilari Bulan Program adli proje tamamlandi.

--- Odevler/AsalSayilariBulanProgram/src/Main.java
+++ Odevler/AsalSayilariBulanProgram/src/Main.java
@@ -2,5 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("1-100 Arasi Asal Sayilari Bulan Programa Hosgeldiniz!");
+
+        System.out.println("1 ile 100 arasindaki asal sayilar asagidakilerdir : ");
+        int bolenSayisi = 0;
+        for (int i = 2; i <= 100; i++) {
+            for (int j = 1; j <= i ; j++) {
+                if (i % j == 0){
+                    bolenSayisi++;
+                }
+            }
+            if (bolenSayisi == 2){
+                System.out.print(i + " ");
+            }
+            bolenSayisi = 0;
+        }
     }
 }
