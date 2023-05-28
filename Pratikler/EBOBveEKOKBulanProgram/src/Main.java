e46d6fec4a759fe575a52bd4e1bd2f3cdfe20ad0
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-28 14:12:38 +0300

    Ebob ve Ekok Bulan Program adli proje tamamlandi.

--- Pratikler/EBOBveEKOKBulanProgram/src/Main.java
+++ Pratikler/EBOBveEKOKBulanProgram/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ilk sayiyi giriniz : ");
+        int ilkSayi = scanner.nextInt();
+        System.out.print("Ikinci sayiyi giriniz : ");
+        int ikinciSayi = scanner.nextInt();
+        int ebob = 1;
+
+        int enBuyuk = 0;
+
+        if (ilkSayi > ikinciSayi) {
+            enBuyuk = ilkSayi;
+        } else {
+            enBuyuk = ikinciSayi;
+        }
+
+        for (int i = 1; i <= enBuyuk; i++) {
+            if (ilkSayi % i == 0 && ikinciSayi % i == 0) {
+                ebob = i;
+            }
+        }
+        System.out.println("Girdiginiz sayilarin ebobu : " + ebob);
+
+        int ekok = 1;
+
+        ekok = (ilkSayi * ikinciSayi) / ebob;
+
+        System.out.println("Girdiginiz sayilarin ekoku : " + ekok);
     }
 }
