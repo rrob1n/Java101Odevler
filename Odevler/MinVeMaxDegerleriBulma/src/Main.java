11802d8a8c8b2c207b2ba9008fe668cfded2efdc
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-28 14:45:13 +0300

    Min ve Max Degerleri Bulma adli proje tamamlandi

--- Odevler/MinVeMaxDegerleriBulma/src/Main.java
+++ Odevler/MinVeMaxDegerleriBulma/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kac tane sayi gireceksiniz : ");
+        int girilenSayi = scanner.nextInt();
+        int enBuyuk = 0;
+        int enKucuk = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= girilenSayi; i++) {
+            System.out.print(i + ". Sayiyi giriniz : ");
+            int sayi = scanner.nextInt();
+
+            if (sayi > enBuyuk) {
+                enBuyuk = sayi;
+            }
+            if (sayi < enKucuk) {
+                enKucuk = sayi;
+            }
+
+            System.out.println(enKucuk);
+        }
+
+        System.out.println("Girdiginiz sayilardan en buyugu : " + enBuyuk);
+        System.out.println("Girdiginiz sayilardan en kucugu : " + enKucuk);
     }
 }
