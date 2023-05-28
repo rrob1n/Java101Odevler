02a5a0fd177b45aaa2cc089f84b1d05bdc0e7e7a
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-28 14:58:29 +0300

    Mukemmel Sayi Bulan Program adli proje tamamlandi.

--- Odevler/MukemmelSayiBulanProgram/src/Main.java
+++ Odevler/MukemmelSayiBulanProgram/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Mukemmel Sayi Bulan Programa Hosgeldiniz!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayi giriniz : ");
+        int birSayi = scanner.nextInt();
+
+        int bolenlerininToplami = 0;
+
+        for (int i = 1; i < birSayi; i++) {
+            if (birSayi % i == 0) {
+                bolenlerininToplami += i;
+            }
+        }
+
+        if (bolenlerininToplami == birSayi) {
+            System.out.println(birSayi + " Mukemmel sayidir");
+        } else {
+            System.out.println(birSayi + " Mukemmel sayi degildir.");
+        }
+
     }
 }
