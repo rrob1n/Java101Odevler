b12061cd0df68bbc5aa7a8c412ae53a406a78244
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-29 22:45:00 +0300

    Fibonacci Serisi adli proje tamamlandi.

--- Odevler/FibonacciSerisi/src/Main.java
+++ Odevler/FibonacciSerisi/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Fibonacci Serisi!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Fibonacci serisinin eleman sayisini giriniz : ");
+        int sayiInput = scanner.nextInt();
+
+        int birinci = 0;
+        int ikinci = 1;
+
+        System.out.print(birinci + " ");
+        for (int i = 0; i < sayiInput; i++) {
+            System.out.print(ikinci + " ");
+            int degistir = birinci + ikinci;
+
+            birinci = ikinci;
+            ikinci = degistir;
+        }
     }
 }
