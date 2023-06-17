5820f355a65b3fde6d69cd44c94cee891df93267
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-17 19:11:51 +0300

    Recursive Ile Fibonacci Serisi Bulan Program adli proje tamamlandi.

--- Pratikler/RecursiveIleFibonacciSerisiBulanProgram/src/Main.java
+++ Pratikler/RecursiveIleFibonacciSerisiBulanProgram/src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        
+public class Main {
+
+    public static int fibonacci(int i) {
+
+        if (i == 1 || i == 2) {
+            return 1;
+        }
+        return fibonacci(i - 1) + fibonacci(i - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Fibonacci serisinin kacinci elemanini bulmak istersin : ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.println(input+". eleman : " + fibonacci(input));
     }
 }
