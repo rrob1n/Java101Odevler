218daccb2fbf88233c448a2021b819b2aa627ff9
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-18 00:39:16 +0300

    Desene Gore Metod Olusturma adli proje tamamlandi.

--- Odevler/DeseneGoreMetotOlusturma/src/Main.java
+++ Odevler/DeseneGoreMetotOlusturma/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void fonksiyonumuz(int sayi) {
+
+        if (sayi <= 0) {
+            System.out.print(sayi + " ");
+        } else {
+            System.out.print(sayi + " ");
+            fonksiyonumuz(sayi - 5);
+            System.out.print(sayi + " ");
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Desene Gore Metot Olusturma");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz : ");
+        int birSayi = scanner.nextInt();
+
+        fonksiyonumuz(birSayi);
     }
 }
