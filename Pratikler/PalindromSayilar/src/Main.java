9c4c8535bba0dade244ba2ac56632d359bfc3df8
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-17 18:48:50 +0300

    PalindromSayilar adli proje tamamlandi.

--- Pratikler/PalindromSayilar/src/Main.java
+++ Pratikler/PalindromSayilar/src/Main.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static boolean isPalindrome(int number) {
+
+        int temp = number, reverseNumber = 0, lastDigit;
+
+        while (temp != 0) {
+            lastDigit = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+            temp /= 10;
+        }
+
+        if (number == reverseNumber) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.print("Lutfen bir sayi giriniz : ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        boolean output = isPalindrome(input);
+
+        if (output) {
+            System.out.println("Girdiginiz sayi palindromdur.");
+        } else {
+            System.out.println("Girdiginiz sayi palindrom degildir.");
+        }
+
+
     }
 }
