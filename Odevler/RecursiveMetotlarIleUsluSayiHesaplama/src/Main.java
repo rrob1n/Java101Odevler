503fa916c630a37a0fa906d9fb993341640f7649
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-17 20:49:07 +0300

    Recursive Metotlar ile Uslu Sayi Hesaplama adli proje tamamlandi.

--- Odevler/RecursiveMetotlarIleUsluSayiHesaplama/src/Main.java
+++ Odevler/RecursiveMetotlarIleUsluSayiHesaplama/src/Main.java
@@ -1,6 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        
+public class Main {
+    public static int Us(int a, int b) {
+        if (b == 0) {
+            return 1;
+        } else {
+            return a * Us(a, b - 1);
+        }
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Recursive Metot ile Us Hesaplama Programina Hosgeldiniz!");
+        System.out.print("Ussunun alinmasini istediginiz sayiyi giriniz : ");
+        int a = scanner.nextInt();
+        System.out.print("Hangi sayiyla ussu alinsin : ");
+        int b = scanner.nextInt();
+
+        int sonuc = Us(a, b);
+        System.out.println("Islemin sonucu : " + sonuc);
+    }
+
 }
