20dab7c87d536df80f5e8b30af1cbc037635c452
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-11 02:31:53 +0300

    Dizideki Elemanlarin Ortalamasini BUlan Program adli proje tamamlandi.

--- Pratikler/DizidekiElemanlarinOrtalamasiniBulanProgram/src/Main.java
+++ Pratikler/DizidekiElemanlarinOrtalamasiniBulanProgram/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // 1.Part ortalama bulma
+
+        int[] sayilar = new int[]{1, 2, 3, 4, 5};
+        double toplam = 0;
+        for (int sayi : sayilar) {
+            toplam += sayi;
+        }
+        double ortalama = toplam / sayilar.length;
+        System.out.println(ortalama);
+
+        // 2.Part harmonik ortalama bulma
+
+        double harmonikToplam = 0;
+
+        for (int i = 0; i < sayilar.length; i++) {
+            for (int j = 1; j <= sayilar[i]; j++) {
+                harmonikToplam += 1.0 / j;
+            }
+        }
+        System.out.println(harmonikToplam);
+
+        double harmonikOrtalama = sayilar.length / harmonikToplam;
+
+        System.out.println(harmonikOrtalama);
+
     }
 }
