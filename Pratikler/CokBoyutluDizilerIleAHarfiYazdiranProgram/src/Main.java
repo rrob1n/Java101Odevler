dde31473509c7777e1490aa6ddeb46c7d938ef06
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-11 13:45:09 +0300

    Cok boyutlu diziler ile a harfi yazdiran program adli proje tamamlandi.

--- Pratikler/CokBoyutluDizilerIleAHarfiYazdiranProgram/src/Main.java
+++ Pratikler/CokBoyutluDizilerIleAHarfiYazdiranProgram/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+        String[][] harf = new String[6][4];
+        for (int i = 0; i < harf.length; i++) {
+            for (int j = 0; j < harf[i].length; j++) {
+                if (i == 0 || i == 2) {
+                    harf[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    harf[i][j] = " * ";
+                } else {
+                    harf[i][j] = "   ";
+                }
+            }
+        }
+        for (String[] satir : harf) {
+            for (String sutun:satir){
+                System.out.print(sutun);
+            }
+            System.out.println();
+        }
     }
 }
